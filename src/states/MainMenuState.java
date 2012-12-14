c46f5e4435504ba87192d85cc8d21422abe5b0f6
R Bonifacio Daitan <heyfudgeda@gmail.com>
2012-12-14 16:26:37 +0800

    Added version 1 of main menu screen + more
    Added MainMenuState to Game
    Rendered background and title in MainMenuState
    Added Graphics: bg_sky and title

--- src/states/MainMenuState.java
+++ src/states/MainMenuState.java
@@ -2,6 +2,7 @@ package states;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
@@ -10,17 +11,25 @@ public class MainMenuState extends BasicGameState
 {
 	int id = 0;
 
+	Image background, title;
+	
+	public MainMenuState(int id)
+	{
+		this.id = id;
+	}
+	
 	@Override
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException 
 	{
-		// TODO Auto-generated method stub
-		
+		background = new Image("Graphics/common/bg_sky.png");
+		title = new Image("Graphics/mainmenu/title.png");
 	}
 
 	@Override
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException 
 	{
-		// TODO Auto-generated method stub
+		background.draw();
+		title.drawCentered(gc.getWidth()/2, gc.getHeight()/2);
 		
 	}
 
